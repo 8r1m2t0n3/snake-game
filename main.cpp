b8a5d65c4213557581c5c1ee87e6f24a509a603d
8r1m2t0n3 <diakov.nv148@gmail.com>
2025-02-10 22:24:26 +0100

    parameters added to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include "snake.h"
 
-int main() {
-	Field field(SMALL);
+int main(int argc, char *argv[]) {
+	if (argc > 2) {
+		std::cout << "Wrong arguments number" << std::endl;
+		return -1;
+	}
+
+	FIELD_SIZE fieldSize = NORMAL;
+
+	if (argc == 2) {
+		int argument = std::stoi(argv[1]);
+		if (argument == 0) {
+			fieldSize = SMALL;
+		} 
+		else if (argument == 1) {
+			fieldSize = NORMAL;
+		}
+		else if (argument == 2) {
+			fieldSize = LARGE;
+		}
+		else {
+			std::cout << "Wrond argument" << std::endl;
+			return -1;
+		}
+	} 
+	Field field(fieldSize);
 	field.start_game();
 	return 0;
 }
