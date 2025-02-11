bbcaae434dca81dcbf4ef2d7bb39cf692536e911
8r1m2t0n3 <diakov.nv148@gmail.com>
2025-02-11 20:16:38 +0100

    main updated

--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
-#include "snake.h"
+#include "game.h"
 
 int main(int argc, char *argv[]) {
 	if (argc > 2) {
-		std::cout << "Wrong arguments number" << std::endl;
+		std::cout << "Wrong number of arguments" << std::endl;
 		return -1;
 	}
 
 	FIELD_SIZE fieldSize = NORMAL;
 
 	if (argc == 2) {
-		int argument = std::stoi(argv[1]);
-		if (argument == 0) {
-			fieldSize = SMALL;
-		} 
-		else if (argument == 1) {
-			fieldSize = NORMAL;
-		}
-		else if (argument == 2) {
-			fieldSize = LARGE;
-		}
-		else {
-			std::cout << "Wrond argument" << std::endl;
-			return -1;
+		switch(std::stoi(argv[1])) {
+			case 0:
+				fieldSize = SMALL;
+				break;
+			case 1:
+				fieldSize = NORMAL;
+				break;
+			case 2:
+				fieldSize = LARGE;
+				break;
+			default:
+				std::cout << "Wrond argument" << std::endl;
+				return -1;
 		}
 	} 
 	Field field(fieldSize);
-	field.start_game();
+	field.startGame();
 	return 0;
 }
